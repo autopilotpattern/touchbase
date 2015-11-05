caacb4901c8768a940ea9bdcfb023261d5ad916d
Tim Gross <tim@0x74696d.com>
2015-11-05 09:19:59 -0500

    Update file path for TB template

--- update-config.sh
+++ update-config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -z "$VIRTUALHOST" ]; then
-    # fetch latest virtualhost template from Consul k/v
-    curl -s --fail consul:8500/v1/kv/touchbase/template?raw > /tmp/virtualhost.ctmpl
+if [ -z "$TB_CONFIG" ]; then
+    # fetch latest config template from Consul k/v
+    curl -s --fail consul:8500/v1/kv/touchbase/template?raw > /tmp/config.json.ctmpl
 else
-    # dump the $VIRTUALHOST environment variable as a file
-    echo $VIRTUALHOST > /tmp/virtualhost.ctmpl
+    # dump the $TB_CONFIG environment variable as a file
+    echo $TB_CONFIG > /tmp/config.json.ctmpl
 fi
 
-# render virtualhost template using values from Consul and reload Touchbase
+# render config template using values from Consul
 consul-template \
     -once \
     -consul consul:8500 \
-    -template "/tmp/virtualhost.ctmpl:/usr/local/lib/node_modules/Couch411/config.json"
+    -template "/tmp/config.json.ctmpl:/usr/local/lib/node_modules/Couch411/config.json"
